821866996135e8a5c6e06000f5b05b2cee40e180
parth-gr <partharora1010@gmail.com>
2025-05-29 13:28:44 +0530

    fix markdown lint issue
    earlier the lint was marked to use a specific
    version, but which was not populated correctly
    No with the fix it will run on the specific
    version and not latest version.
    
    Signed-off-by: parth-gr <partharora1010@gmail.com>

--- hack/install-markdownlint.sh
+++ hack/install-markdownlint.sh
@@ -2,4 +2,4 @@
 
 dnf -y module enable nodejs:12
 dnf -y install nodejs
-npm install -g markdownlint@0.26.0 markdownlint-cli2@0.4.0
+npm install markdownlint@0.26.0 markdownlint-cli2@0.4.0 --save-dev
